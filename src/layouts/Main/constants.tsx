a73f25c0bfabb56213849bade9543f36b5ab6db6
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-02 00:10:11 +0000

    prettier

--- src/layouts/Main/constants.tsx
+++ src/layouts/Main/constants.tsx
@@ -1,6 +1,6 @@
 export const drawerWidth = 240;
 export const responsiveLimit = 800;
 export const linkStyle = {
-    color: '#2784a0',
-    'text-decoration': 'none',
-  };
+  color: '#2784a0',
+  'text-decoration': 'none',
+};
