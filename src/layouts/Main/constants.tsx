61dfdd3c7d8051eaaa8c64c9bac445f2f8b1538d
Noofreuuuh <dev.n2ofr@gmail.com>
2022-10-02 00:16:41 +0000

    Fix sidepanel responsive limit

--- src/layouts/Main/constants.tsx
+++ src/layouts/Main/constants.tsx
@@ -1,5 +1,5 @@
 export const drawerWidth = 240;
-export const responsiveLimit = 800;
+export const responsiveLimit = 900;
 export const linkStyle = {
   color: '#2784a0',
   'text-decoration': 'none',
