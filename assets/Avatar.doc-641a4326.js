b2a9fbdb52aa084acad7bb001db3cc25d1706fbd
Noofreuuuh <dev.n2ofr@gmail.com>
2023-09-18 01:10:07 +0200

    Updates

--- assets/Avatar.doc-641a4326.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-65670fe4.js";import{M as i,P as e,C as a}from"./index-1d757d70.js";import{A as m}from"./Avatar.stories-2477c0ab.js";import{u as s}from"./index-ef048a71.js";import"./index-402471b7.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-3dd789df.js";import"../sb-preview/runtime.js";import"./index-2a8f040d.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./index-d102b708.js";function o(n){const r=Object.assign({h1:"h1",p:"p",h2:"h2"},s(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:m}),`
-`,t.jsx(r.h1,{id:"avatar",children:"Avatar"}),`
-`,t.jsx(r.p,{children:"An avatar is ..."}),`
-`,t.jsx(e,{}),`
-`,t.jsx(r.h2,{id:"props",children:"Props"}),`
-`,t.jsx(a,{}),`
-`,t.jsx(r.h2,{id:"stories",children:"Stories"})]})}function v(n={}){const{wrapper:r}=Object.assign({},s(),n.components);return r?t.jsx(r,Object.assign({},n,{children:t.jsx(o,n)})):o(n)}export{v as default};
-//# sourceMappingURL=Avatar.doc-641a4326.js.map
