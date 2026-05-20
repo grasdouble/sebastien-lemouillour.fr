9960e5ae46949e572f20efc62a0e4cc6c2723d9c
Noofreuuuh <dev.n2ofr@gmail.com>
2026-05-21 01:10:40 +0200

    fix: missing change in importmap

--- packages/slm-container/src/main.ts
+++ packages/slm-container/src/main.ts
@@ -21,42 +21,9 @@ const loadApp =
 
 // PARCELS
 registerApplication({
-  name: '@grasdouble/lufa_microfrontend_home',
-  app: loadApp('@grasdouble/lufa_microfrontend_home'),
+  name: '@grasdouble/slm_parcel_landing-page',
+  app: loadApp('@grasdouble/slm_parcel_landing-page'),
   activeWhen: (location: Location) => location.pathname === '/',
-});
-
-registerApplication({
-  name: '@grasdouble/lufa_design-system-storybook',
-  app: () => {
-    // Create a false module to encapsulate your storybook application
-    return Promise.resolve({
-      bootstrap: (): Promise<void> => Promise.resolve(),
-      mount: (): Promise<void> => {
-        // Create an iframe to encapsulate the storybook application
-        const iframe = document.createElement('iframe');
-        iframe.id = 'storybook-iframe'; // Add an ID for easier unmounting
-        iframe.src = 'https://lufa-storybook.sebastien-lemouillour.fr';
-        iframe.style.width = '100%';
-        iframe.style.height = '100vh';
-        iframe.style.border = 'none';
-        const appElement = document.getElementById('app');
-        if (appElement) {
-          appElement.appendChild(iframe);
-        }
-        return Promise.resolve();
-      },
-      unmount: (): Promise<void> => {
-        // Unmount the iframe and remove it from the DOM
-        const iframe = document.getElementById('storybook-iframe');
-        if (iframe) {
-          iframe.remove();
-        }
-        return Promise.resolve();
-      },
-    });
-  },
-  activeWhen: ['/storybook'], // Active when the URL is /storybook
 });
 
 start();
