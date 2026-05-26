76de81924cabd7bc5c03086834f956f487137fd7
Noofreuuuh <dev.n2ofr@gmail.com>
2026-05-26 02:50:47 +0200

    fix: improve regex for extracting attributes in setMeta function

--- packages/shared/src/usePageSeo.ts
+++ packages/shared/src/usePageSeo.ts
@@ -10,7 +10,10 @@ function setMeta(selector: string, attr: string, content: string) {
   let el = document.querySelector<HTMLMetaElement>(selector);
   if (!el) {
     el = document.createElement('meta');
-    const [attrName, attrValue] = selector.replace('meta[', '').replace(']', '').split('=');
+    const [attrName, attrValue] = selector
+      .replace(/^meta\[/, '')
+      .replace(/\]/g, '')
+      .split('=');
     el.setAttribute(attrName, attrValue.replace(/"/g, ''));
     document.head.appendChild(el);
   }
