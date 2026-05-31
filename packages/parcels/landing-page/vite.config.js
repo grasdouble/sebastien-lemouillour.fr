e889dc361814447dac74fc8d92a1336517c8decc
Noofreuuuh <dev.n2ofr@gmail.com>
2026-05-31 22:46:08 +0200

    refactor: move CDN dependencies to peerDependencies and update externalizeDeps configuration

--- packages/parcels/landing-page/vite.config.js
+++ packages/parcels/landing-page/vite.config.js
@@ -11,11 +11,10 @@ export default defineConfig({
     cssInjectedByJsPlugin(),
     sitemapPlugin({ urls: [{ loc: '/', changefreq: 'monthly', priority: '1.0' }] }),
     externalizeDeps({
-      deps: true,
+      deps: false,
       devDeps: false,
       optionalDeps: false,
-      peerDeps: false,
-      except: ['@grasdouble/slm_shared'],
+      peerDeps: true,
       nodeBuiltins: true,
     }),
   ],
